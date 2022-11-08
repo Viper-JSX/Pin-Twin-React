05c516d7a98cc0c3707e7e1ef8821533813deb95
Yura_viper <yuramcpecs@gmail.com>
2022-11-08 15:20:36 +0100

    Created notifications variable inside NotificationsWindow to retrieve and display the notifications

--- src/components/Header/Notifications_window/Notifications_window.js
+++ src/components/Header/Notifications_window/Notifications_window.js
@@ -1,8 +1,14 @@
+import { useSelector } from "react-redux";
+
 function NotificationsWindow( { notificationsWindowVisibility, notificationsWindowRef, handleNotificationsHide } ){
+    const notifications = useSelector((state) => state.user.notifications) || [];
 
     return(
         <div className={`notificationsWindow ${notificationsWindowVisibility}`} ref={notificationsWindowRef} tabIndex={1} onBlur={handleNotificationsHide}>
             <b>Notifications</b>
+            {
+                notifications.map((notification) => console.log(notification))
+            }
         </div>
     );  
 }
