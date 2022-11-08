fb48c20ba0db4453bbea9316d66b6d4d674c8ad3
Yura_viper <yuramcpecs@gmail.com>
2022-11-08 15:49:07 +0100

    Implemented MessagesWindow bases on the NotificationsWindow logic

---
+++ src/components/Header/Messages_window/Messages_window.js
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+
+function MessagesWindow( { messagesWindowVisibility, messagesWindowRef, handleMessagesHide } ){
+    const messages = useSelector((state) => state.user.conversations) || [];
+
+    return(
+        <div className={`messagesWindow ${messagesWindowVisibility}`} ref={messagesWindowRef} tabIndex={1} onBlur={handleMessagesHide}>
+            <b>Messages</b>
+            {
+                messages.map((conversation) => console.log(conversation))
+            }
+        </div>
+    );  
+}
+
+export default MessagesWindow;
--- src/components/Header/Open_messages.js
+++ src/components/Header/Open_messages.js
@@ -1,8 +1,36 @@
+import { useRef, useState } from "react";
+import { createPortal } from "react-dom";
+import MessagesWindow from "./Messages_window/Messages_window";
+
 function OpenMessages(){
+    const messagesWindowRef = useRef(null);
+    const [ messagesWindowVisibility, setMessagesWindowVisibility ] = useState("hidden");
+
+    function handleMessagesShow(){
+        messagesWindowRef.current.style.display = "block";
+        messagesWindowRef.current.focus();
+        setMessagesWindowVisibility("visible");
+    }
+
+    function handleMessagesHide(){
+        setMessagesWindowVisibility("hidden");
+        setTimeout(() => messagesWindowRef.current.style.display = "none", 1000)
+    }
+
     return(
-        <button>
-            <img src="" alt="Show messages" />
-        </button>
+        <>
+            <button onClick={handleMessagesShow}>
+                g
+                <img src="" alt="Show messages" />
+            </button>
+
+            {
+                createPortal(
+                    <MessagesWindow messagesWindowVisibility={messagesWindowVisibility} messagesWindowRef={messagesWindowRef} handleMessagesHide={handleMessagesHide} />,
+                    document.body
+                )
+            }
+        </>
     );
 }
 
