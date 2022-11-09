eb9f8b1eba4fe46ed3957de0c2a6d9755b2822d7
Yura_viper <yuramcpecs@gmail.com>
2022-11-09 10:22:58 +0100

    Added 3 user actions

--- src/redux/action_types.js
+++ src/redux/action_types.js
@@ -2,6 +2,11 @@
 export const LOGIN = "LOGIN";
 export const REGISTER = "REGISTER";
 
+export const CREATE_PIN = "CREATE_PIN";
+export const EDIT_PIN = "EDIT_PIN";
+export const DELETE_PIN = "DELETE_PIN";
+
+
 /*---Search---*/
 export const CHANGE_PINS_SEARCH_TERM = "CHANGE_PINS_SEARCH_TERM";
 export const CHANGE_CONVERSATIONS_SEARCH_TERM = "CHANGE_CONVERSATIONS_SEARCH_TERM";
