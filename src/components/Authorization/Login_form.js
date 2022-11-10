393876e29c7e3b9dfd068b48496ac291dc29f319
Yura_viper <yuramcpecs@gmail.com>
2022-11-10 18:27:28 +0100

    Inside LoginForm created 2 inputs for email and password, and declared state for holding current values

--- src/components/Authorization/Login_form.js
+++ src/components/Authorization/Login_form.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import CloseButton from "../Reusable_components/Close_button";
 
 function LoginForm({ handleAuthorizationWindowClose }){
-    
+    const [ loginData, setLoginData ] = useState({ login: "", password: "" });
+
     return(
         <div className="loginForm">
             <CloseButton handler={handleAuthorizationWindowClose} />
             <form>
-                Login
+                <input type="email" value={loginData.login} />
+                <input type="password" value={loginData.password} />
             </form>
         </div>
     );
