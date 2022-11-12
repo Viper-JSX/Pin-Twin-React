c20513896244ad0459fbafbd7bac2212887a5232
Yura_viper <yuramcpecs@gmail.com>
2022-11-12 19:51:22 +0100

    Added conditional rendering to MainPage, if user is logged in then pins are shown if user not logged in then some other content will be rendered

--- src/components/Main_page/Main_page.js
+++ src/components/Main_page/Main_page.js
@@ -1,11 +1,20 @@
+import { useSelector } from "react-redux/es/exports";
+
 import Pins from "../Pins/Pins";
 
 function MainPage(){
+    const user = useSelector((state) => state.user);
+
     return(
         <div id="mainPage">
-            Main Main
+            Main
 
-            <Pins pins={[]} />
+            {
+                user ? 
+                <Pins pins={[]} />
+                :
+                "Some other content"
+            }
         </div>
     );
 }
