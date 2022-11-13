400c77803255598c3fcaaa623024ee79bab395a9
Yura_viper <yuramcpecs@gmail.com>
2022-11-13 20:09:44 +0100

    Added hadnlers to hadnle editor data change

--- src/components/Pin_editor/Pin_editor.js
+++ src/components/Pin_editor/Pin_editor.js
@@ -5,12 +5,33 @@ function PinEditor({ mode, handlePinCreate, handlePinEdit }){
     const location = useLocation();
     const [ pinData, setPinData ] = useState(location?.state?.pin || { imageSrc: "", title: "", discription: "", tagsString: "" });
 
+    function handlePinImageChange(event){
+        const reader = new FileReader();
+        reader.readAsDataURL(event.target.files[0])
+        reader.onload = function(processed){
+            console.log(processed.srcElement.result);
+            setPinData({ ...pinData, imageSrc: processed.srcElement.result });
+        }
+    }
+
+    function handleTitleChange(event){
+        setPinData({ ...pinData, title: event.target.value });
+    }
+
+    function handleDiscriptionChange(event){
+        setPinData({ ...pinData, discription: event.target.value });
+    }
+
+    function handleTagsChange(event){
+        setPinData({ ...pinData, tagsString: event.target.value });
+    }
+
     return(
         <div className="pinEditor">
-            <input type="file" accept="image/png, image/gif, image/jpeg" value={pinData.imageSrc}  />
-            <input type="text" value={pinData.title} placeholder="Title" />
-            <textarea value={pinData.discription} placeholder="Discription"></textarea>
-            <input type="text" value={pinData.tagsString} placeholder="Tags" />
+            <input type="file" accept="image/png, image/gif, image/jpeg" onChange={handlePinImageChange} />
+            <input type="text" value={pinData.title} placeholder="Title" onChange={handleTitleChange} />
+            <textarea value={pinData.discription} placeholder="Discription" onChange={handleDiscriptionChange} ></textarea>
+            <input type="text" value={pinData.tagsString} placeholder="Tags" onChange={handleTagsChange} />
             {
                 mode === "create" ?
                 <button className="createPin" onClick={() => handlePinCreate()}>Create</button>
