31334f678b18e0f6280a6901832ae473f6bf846a
Yura_viper <yuramcpecs@gmail.com>
2022-11-13 18:33:22 +0100

    Similar pins are filtered based on whether pins have at least one similar tag

--- src/components/Pins/Pin/Pin.js
+++ src/components/Pins/Pin/Pin.js
@@ -1,15 +1,28 @@
 import { useLocation } from "react-router";
+import { pins } from "../../../various_things/pins";
 import PinInfo from "./Pin_info";
+import Pins from "../Pins";
 
 function Pin(){
     const location = useLocation();
     const pin = location.state.pin;
-    console.log(pin)
+
+    const similarPins = pins.filter((possibleSimilarPin) => {
+        for(let i = 0; i < pin.tags.length; i++){
+            if(possibleSimilarPin.tags.includes(pin.tags[i]) && possibleSimilarPin.id !== pin.id){
+                return true;
+            }
+        }
+    });
+    console.log("Similar",similarPins);
 
     return(
         <div className="pin">
             <img src={pin.imageSrc} alt={`${[pin.title]} image`} />
             <PinInfo pin={pin} />
+
+            <b>Similar pins</b>
+            <Pins pins={similarPins} />
         </div> 
     );  
 }
