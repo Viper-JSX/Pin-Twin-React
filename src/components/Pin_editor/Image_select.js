1dd2f67f90382514779f49d37b79fead3a5fa4e2
Yura_viper <yuramcpecs@gmail.com>
2022-11-15 20:42:34 +0100

    Added styles (flex...) to PinEditorInputs and ImageSelect

--- src/components/Pin_editor/Image_select.js
+++ src/components/Pin_editor/Image_select.js
@@ -1,6 +1,6 @@
 function ImageSelect({ imageSrc, handlePinImageChange }){
     return(
-        <label>
+        <label className="imageSelect">
             <input type="file" accept="image/png, image/gif, image/jpeg" onChange={handlePinImageChange} />
             <img src={imageSrc} alt="Pin image" />
         </label>
