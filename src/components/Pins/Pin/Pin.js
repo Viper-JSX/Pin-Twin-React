f9a54a8628572fb2da9da7136953fd3ab5ab1b8e
Yura_viper <yuramcpecs@gmail.com>
2022-11-15 14:32:36 +0100

    Edit button is displayed only if user is creator of the pin

--- src/components/Pins/Pin/Pin.js
+++ src/components/Pins/Pin/Pin.js
@@ -1,12 +1,18 @@
 import { useLocation } from "react-router";
+import { useSelector } from "react-redux/es/exports";
+
 import { pins } from "../../../various_things/pins";
+
 import PinInfo from "./Pin_info";
 import Pins from "../Pins";
 import OpenPinEditor from "./Open_pin_editor";
 
 function Pin({ handleConfirmationWindowOpen }){
+    const user = useSelector((state) => state.user);
     const location = useLocation();
     const pin = location.state.pin;
+
+    console.log(user.id === pin.creatorId )
 
     const similarPins = pins.filter((possibleSimilarPin) => {
         for(let i = 0; i < pin.tags.length; i++){
@@ -18,7 +24,12 @@ function Pin({ handleConfirmationWindowOpen }){
 
     return(
         <div className="pin">
-            <OpenPinEditor pin={pin} />
+            {
+                (user.id || user.id === 0) && user.id === pin.creatorId ?
+                <OpenPinEditor pin={pin} />
+                :
+                null
+            }
             <img src={pin.imageSrc} alt={`${[pin.title]} image`} />
             <PinInfo pin={pin} />
 
--- src/various_things/pins.js
+++ src/various_things/pins.js
@@ -2,6 +2,6 @@ import { PinClass } from '../Classes/Pin_class';
 
 export const pins = [
     new PinClass(0, "https://petapixel.com/assets/uploads/2022/07/DALLEcopy.jpg", "First pin", "The pin that will become a history", ["life", "world"], new Date()),
-    new PinClass(0, "", "Second pin", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(1, "", "Second pin", "The pin that will become a history", ["sky", "bottom"], new Date()),
     new PinClass(0, "", "Third pin", "The pin that will become a history", ["life", "world"], new Date())
 ]
