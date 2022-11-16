5f9cbece3f8ad7517ba66faae2d8161db0f77e8c
Yura_viper <yuramcpecs@gmail.com>
2022-11-16 10:20:21 +0100

    Removed a space

--- src/components/User_profile/User_profile.js
+++ src/components/User_profile/User_profile.js
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router";
+import { USER_PINS } from "../../various_things/constant_keywords";
 
 function UserProfileWindow(){
     const location = useLocation();
@@ -8,8 +9,8 @@ function UserProfileWindow(){
     const otherUser = location.state.user; //Other person's profile
     
     const otherUserProfile = Boolean(otherUser); //indicates whether user viewing own profile or other person's profile
-    const currentlyShownPins = useState(null)
-
+    const currentlyShownPins = useState(USER_PINS);
+    
     return(
         <div className="userProfileWindow"> 
 
