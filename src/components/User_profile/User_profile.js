0ac018b69e41147ecb1b4965de030dfea32c3643
Yura_viper <yuramcpecs@gmail.com>
2022-11-16 09:41:19 +0100

    User is retrieved throught redux or router state, created varible indicating whether user viewing his own profile or other person's profile

--- src/components/User_profile/User_profile.js
+++ src/components/User_profile/User_profile.js
@@ -1,12 +1,19 @@
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router";
 
 function UserProfileWindow(){
-    const user = useSelector((state) => state.user);
-    console.log(user);
+    const location = useLocation();
+
+    const user = useSelector((state) => state.user); //User itself
+    const otherUser = location.state.user; //Other person's profile
+    
+    const otherUserProfile = Boolean(otherUser); //indicates whether user viewing own profile or other person's profile
+    const currentlyShownPins = useState(null)
 
     return(
-        <div className="userProfileWindow">
-            User profile
+        <div className="userProfileWindow"> 
+
+
         </div>
     );
 }
