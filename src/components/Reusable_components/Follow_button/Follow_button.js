4a94bcfcf9ad613c01fdb43b584c6779a4733035
Yura_viper <yuramcpecs@gmail.com>
2022-11-18 14:43:39 +0100

    Two buttons are conditionally rendered based on whether user already following or not

--- src/components/Reusable_components/Follow_button/Follow_button.js
+++ src/components/Reusable_components/Follow_button/Follow_button.js
@@ -1,14 +1,23 @@
 const { useSelector } = require("react-redux");
 
-function FollowButton({ userToFollowOrUnfollowId, handleFollow, handleUnFollow }){
-    const [userId, userFollowings] = useSelector((state) => [ state.user.id, state.user.followings ]);
-
+function FollowButton({ userToFollowOrUnfollowId, handleFollow, handleUnfollow }){
+    const user = useSelector((state) => state.user);
+    const isAlreadyFollowing = user && user.followings.includes(userToFollowOrUnfollowId); //Whether user is logged in and whether user already follows
 
     return(
         <>
             {
-
+                isAlreadyFollowing ? 
+                <button className="followButton" onClick={() => handleFollow({ userToFollowId: userToFollowOrUnfollowId })}>
+                    Follow
+                </button>
+                :
+                <button className="unfollowButton" onClick={() => handleUnfollow({ userToUnfollowId: userToFollowOrUnfollowId })}>
+                    Unfollow
+                </button>
             }
         </>
     );
 }
+
+export default FollowButton;
