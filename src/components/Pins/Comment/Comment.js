db2d3a194534a630c77f9124c190e564722b8341
Yura_viper <yuramcpecs@gmail.com>
2022-11-19 22:21:20 +0100

    Created Comment component which represents comment text and author, also has delete button which passed up the pinId and commentId to find comment to delete

---
+++ src/components/Pins/Comment/Comment.js
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { users } from "../../../various_things/users";
+
+function Comment({ comment, handleUserDelete }){
+    const userId = useSelector((state) => state.user.id);
+    const commentAuthor = users.find((user) => user.id === comment.creatorId);
+
+    return(
+        <div className="comment">
+            {
+                userId === comment.creatorId ? 
+                <button class="deleteComment" onClick={() => handleCommentDelete({ pinId: comment.pinId, commentId: comment.id })}>Delete</button>
+                :
+                null
+            }
+
+            <p className="commentText">{comment.text}</p>
+            <NavLink>
+                <b className="commentAuthor">{commentAuthor.nickname}</b>
+            </NavLink>
+        </div>
+    );
+}
+
+export default Comment;
