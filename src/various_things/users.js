12f5b9021d675d5aa9418792c29802bdf7dd3529
Yura_viper <yuramcpecs@gmail.com>
2022-11-20 18:38:35 +0100

    Created pins

--- src/various_things/users.js
+++ src/various_things/users.js
@@ -1,11 +1,51 @@
-import { UserClass } from "../Classes/User_class";
-import { pins } from "./pins";
+import { PinClass } from '../Classes/Pin_class';
 
-export const users = [
-    new UserClass("yuramcpecs@gmail.com", "be"),
-    new UserClass("ivanzvonarpivo@gmail.com", "e"),
-    new UserClass("yurkig@gmail.com", "b")
-];
+export const pins = [
+    new PinClass(0, "https://petapixel.com/assets/uploads/2022/07/DALLEcopy.jpg", "Pin 1", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 2", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 3", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(0, "https://petapixel.com/assets/uploads/2022/07/DALLEcopy.jpg", "Pin 4", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 5", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 6", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(0, "https://petapixel.com/assets/uploads/2022/07/DALLEcopy.jpg", "Pin 7", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 8", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 9", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(0, "https://petapixel.com/assets/uploads/2022/07/DALLEcopy.jpg", "Pin 10", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 11", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 12", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(0, "https://petapixel.com/assets/uploads/2022/07/DALLEcopy.jpg", "Pin 13", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 14", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 15", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(0, "https://petapixel.com/assets/uploads/2022/07/DALLEcopy.jpg", "Pin 16", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 17", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 18", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(0, "https://petapixel.com/assets/uploads/2022/07/DALLEcopy.jpg", "Pin 19", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 20", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 21", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(0, "https://petapixel.com/assets/uploads/2022/07/DALLEcopy.jpg", "Pin 22", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 23", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 24", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(0, "https://petapixel.com/assets/uploads/2022/07/DALLEcopy.jpg", "Pin 25", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 26", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 27", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 28", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 29", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 30", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 31", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 32", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 33", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 34", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 35", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 36", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 37", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 38", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 39", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 40", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 41", "The pin that will become a history", ["life", "world"], new Date()),
+    new PinClass(1, "", "Pin 42", "The pin that will become a history", ["sky", "bottom"], new Date()),
+    new PinClass(2, "", "Pin 43", "The pin that will become a history", ["life", "world"], new Date()),
+    
+]
 
 users[0].pins[0] = pins[0];
 users[1].pins[0] = pins[1];
