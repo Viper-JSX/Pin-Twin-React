c180170e2150032fae887405e30a2ea888173302
Yura_viper <yuramcpecs@gmail.com>
2022-11-21 20:05:04 +0100

    Added 'selected' class to ImageSelect based on whether image is selected, added hover effects based on whether image is selected or not, configured width of ImageSelect

--- src/components/Pin_editor/Image_select.js
+++ src/components/Pin_editor/Image_select.js
@@ -1,6 +1,7 @@
 function ImageSelect({ imageSrc, handlePinImageChange }){
     return(
-        <label className="imageSelect">
+        <label className={`imageSelect ${imageSrc ? "selected" : ""}`} >
+            <div className="imageSelectCover">Select Image <br /></div>
             <input type="file" accept="image/png, image/gif, image/jpeg" onChange={handlePinImageChange} />
             <img src={imageSrc} alt="Pin image" />
         </label>
