741d3407c3012f9e1b31ce2eb1419a52985ea684
Yura_viper <yuramcpecs@gmail.com>
2022-11-21 22:11:05 +0100

    Added flex layout to PinContent, added className to pin title and discription elements

--- src/components/Pins/Pin/Pin_info.js
+++ src/components/Pins/Pin/Pin_info.js
@@ -3,8 +3,8 @@ import Comments from "../../Reusable_components/Comments/Comments";
 function PinInfo({ pin }){
     return(
         <div className="pinInfo">
-            <b>{ pin.title }</b>
-            <p>{ pin.discription }</p>
+            <b className="pinTitle">{ pin.title }</b>
+            <p className="pinDiscription">{ pin.discription }</p>
         </div>
     );
 }
