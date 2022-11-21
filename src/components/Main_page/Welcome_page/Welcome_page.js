b5d51bf1e3c2972d079b8324adff8677b9ebbffa
Yura_viper <yuramcpecs@gmail.com>
2022-11-21 14:53:36 +0100

    Added div to cover entire WelcomePage

--- src/components/Main_page/Welcome_page/Welcome_page.js
+++ src/components/Main_page/Welcome_page/Welcome_page.js
@@ -3,8 +3,10 @@ import OpenAuthorization from "../../Authorization/Open_authorization/Open_autho
 function WelcomePage({ handleLoginFormOpen, handleSignUpFormOpen }){
     return(
         <div className="welcomePage">
-            <h2>Welcome to PinTwin</h2>
-            <OpenAuthorization handleLoginFormOpen={handleLoginFormOpen} handleSignUpFormOpen={handleSignUpFormOpen} />
+            <div className="welcomePageCover">
+                <h2>Welcome to PinTwin</h2>
+                <OpenAuthorization handleLoginFormOpen={handleLoginFormOpen} handleSignUpFormOpen={handleSignUpFormOpen} />
+            </div>
         </div>
     );
 }
