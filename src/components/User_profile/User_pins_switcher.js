e93b9ceb60c3eb2c815df984ed588ed5b35b4564
Yura_viper <yuramcpecs@gmail.com>
2022-11-21 17:56:45 +0100

    Added styling to SavedPins as in Pin component. Added styling to FileEditButton and some hover and transition effects

--- src/components/User_profile/User_pins_switcher.js
+++ src/components/User_profile/User_pins_switcher.js
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import { SAVED_PINS, USER_PINS } from "../../various_things/constant_keywords";
 
 function UserPinsSwitcher({ typeOfPinsToShow, handleUserPinsSwitch }){
-    console.log(typeOfPinsToShow)
     return(
         <div className="userPinsSwitcher">
             <Link className={`${typeOfPinsToShow === USER_PINS ? "active" : ""}`} to="" onClick={() => handleUserPinsSwitch(USER_PINS)}>Pins</Link>
