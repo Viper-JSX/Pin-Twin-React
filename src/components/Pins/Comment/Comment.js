635ea8118be80da7eebace0cef392715b314f028
Yura_viper <yuramcpecs@gmail.com>
2022-11-21 12:02:25 +0100

    Comment delete button moved to the bottom

--- src/components/Pins/Comment/Comment.js
+++ src/components/Pins/Comment/Comment.js
@@ -5,17 +5,16 @@ function Comment({ userId, comment, handleCommentDelete }){
     const commentAuthor = users.find((user) => user.id === comment.authorId);
     return(
         <div className="comment">
+            <p className="commentText">{comment.text}</p>
+            <NavLink>
+                <b className="commentAuthor">{commentAuthor.nickname}</b>
+            </NavLink>
             {
                 userId === comment.authorId ? 
                 <button className="deleteComment" onClick={() => handleCommentDelete({ pinId: comment.pinId, commentId: comment.id })}>Delete</button>
                 :
                 null
             }
-
-            <p className="commentText">{comment.text}</p>
-            <NavLink>
-                <b className="commentAuthor">{commentAuthor.nickname}</b>
-            </NavLink>
         </div>
     );
 }
