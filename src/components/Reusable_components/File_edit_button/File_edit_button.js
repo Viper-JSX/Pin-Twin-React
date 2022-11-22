1e5969e695b9753d7037c11338296f686094db85
Yura_viper <yuramcpecs@gmail.com>
2022-11-22 21:32:45 +0100

    Added font-awesome icon form FileEditButton

--- src/components/Reusable_components/File_edit_button/File_edit_button.js
+++ src/components/Reusable_components/File_edit_button/File_edit_button.js
@@ -1,8 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+
 function FileEditButton({ accept, handler }){
     return(
         <label className="fileEditButton" >
             <input type="file" accept={accept} hidden onChange={handler} />
-            Edit
+            <FontAwesomeIcon icon={faEdit} />
         </label>
     );
 }
