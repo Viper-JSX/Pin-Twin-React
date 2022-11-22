c8260a5f462bd22cac64f0b545fda152a011032f
Yura_viper <yuramcpecs@gmail.com>
2022-11-22 18:06:44 +0100

    Added className to OpenPinEditor, added styles like in RemovePinFromSaved

--- src/components/Pins/Pin/Open_pin_editor.js
+++ src/components/Pins/Pin/Open_pin_editor.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function OpenPinEditor({ pin }){
     return(
         <NavLink to="edit" state={{ pin }}>
-            <button>Edit</button>
+            <button className="openPinEditor">Edit</button>
         </NavLink>
     );
 }
