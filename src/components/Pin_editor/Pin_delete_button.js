5d3946c967281238e7a02db3ea3acd50e60390b6
Yura_viper <yuramcpecs@gmail.com>
2022-11-22 21:18:05 +0100

    Added id to pin delete button

--- src/components/Pin_editor/Pin_delete_button.js
+++ src/components/Pin_editor/Pin_delete_button.js
@@ -2,7 +2,7 @@ import { CONFIRM_PIN_DELETE } from "../../confirmation_types/confirmation_types"
 
 function PinDeleteButton({ pinId, handleConfirmationWindowOpen }){
     return (
-        <button onClick={() => handleConfirmationWindowOpen({ confirmationType: CONFIRM_PIN_DELETE, pinId, confirmationText: "Do you really want to delete this pin?"  })}>Delete</button>
+        <button className="pinDeleteButton" onClick={() => handleConfirmationWindowOpen({ confirmationType: CONFIRM_PIN_DELETE, pinId, confirmationText: "Do you really want to delete this pin?"  })}>Delete</button>
     );
 }
 
