84da9151c9bf812a9aad4483186d24f8f482ce73
Yura_viper <yuramcpecs@gmail.com>
2022-11-22 19:36:50 +0100

    Configured teaxarea in PinEditor, made corners rounded

--- src/components/Pin_editor/Pin_editor_text_fields.js
+++ src/components/Pin_editor/Pin_editor_text_fields.js
@@ -2,7 +2,7 @@ function PinEditorTextFields({ pinData, handlePinTitleChange, handlePinDiscripti
     return(
         <div className="pinEditorTextFields">
             <input type="text" value={pinData.title} placeholder="Title" onChange={handlePinTitleChange} />
-            <textarea value={pinData.discription} placeholder="Discription" onChange={handlePinDiscriptionChange} ></textarea>
+            <textarea value={pinData.discription} cols={50} rows={10} placeholder="Discription" onChange={handlePinDiscriptionChange} ></textarea>
             <input type="text" value={pinData.tagsString} placeholder="Tags" onChange={handlePinTagsChange} />
         </div>
     );
