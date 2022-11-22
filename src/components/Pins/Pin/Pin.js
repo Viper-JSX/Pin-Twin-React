fc710b3db9ebffcc23a2617dcbd0ce36045c082f
Yura_viper <yuramcpecs@gmail.com>
2022-11-22 19:52:11 +0100

    Opps, I've forgotten the condition

--- src/components/Pins/Pin/Pin.js
+++ src/components/Pins/Pin/Pin.js
@@ -9,17 +9,15 @@ import PinContent from "./Pin_content";
 
 function Pin({ handlePinSave, handlePinRemoveFromSaved, handlePinOpenerClick, handleCommentCreate, handleCommentDelete, handleLoginFormOpen }){
     const [ searchParams, setSearchParams ] = useSearchParams();
-
     const pinId = parseInt(searchParams.get("id"));
     const pin = useSelector((state) => state.app.allPins?.find((pin) => pin.id === pinId)); //Getting pin based on param 'id'
+    const similarPins = []; 
 
     if(!pin){
         return(
             <Navigate to="/" />
         );
     }
-
-    const similarPins = []; 
     
     for(let i = 0; i < pins.length; i++){
         for(let j = 0; j < pin.tags.length; j++){
@@ -28,8 +26,12 @@ function Pin({ handlePinSave, handlePinRemoveFromSaved, handlePinOpenerClick, ha
                 break;
             }
         }
+
+        if(similarPins.length >= 10){
+            break;
+        }
     }
-    
+
     return(
         <div className="pin">
             <PinContent pin={pin} handlePinSave={handlePinSave} handlePinRemoveFromSaved={handlePinRemoveFromSaved} handleCommentCreate={handleCommentCreate} handleCommentDelete={handleCommentDelete} handleLoginFormOpen={handleLoginFormOpen} />
