508c1e3604e1a596b64c9c05cbba62149e7768ce
Yura_viper <yuramcpecs@gmail.com>
2022-11-23 11:33:02 +0100

    Added useEffect to Pin, when user opens another pin then pin is scrolled to top

--- src/components/Pins/Pin/Pin.js
+++ src/components/Pins/Pin/Pin.js
@@ -6,12 +6,18 @@ import { pins } from "../../../various_things/pins";
 
 import Pins from "../Pins";
 import PinContent from "./Pin_content";
+import { useEffect } from "react";
 
 function Pin({ handlePinSave, handlePinRemoveFromSaved, handlePinOpenerClick, handleCommentCreate, handleCommentDelete, handleLoginFormOpen }){
     const [ searchParams, setSearchParams ] = useSearchParams();
     const pinId = parseInt(searchParams.get("id"));
     const pin = useSelector((state) => state.app.allPins?.find((pin) => pin.id === pinId)); //Getting pin based on param 'id'
     const similarPins = []; 
+
+    useEffect(() => {
+        console.log("render");
+        window.scrollTo(0, 0);
+    }, [pinId]);
 
     if(!pin){
         return(
