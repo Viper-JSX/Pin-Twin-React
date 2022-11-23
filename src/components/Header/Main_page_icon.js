a019e7ad829c75b2bc9ff099322256096c7b50e3
Yura_viper <yuramcpecs@gmail.com>
2022-11-23 11:53:32 +0100

    Added icon to MainPageIcon but it's not working

--- src/components/Header/Main_page_icon.js
+++ src/components/Header/Main_page_icon.js
@@ -1,9 +1,10 @@
 import { NavLink } from "react-router-dom";
 
+
 function MainPageIcon(){
     return(
         <NavLink to="/">
-            <img className="mainPageIcon" alt="Main page" />
+            <img className="mainPageIcon" src={"../../files/images/app_icon.svg"} alt="Main page" />
         </NavLink>
     );
 }
