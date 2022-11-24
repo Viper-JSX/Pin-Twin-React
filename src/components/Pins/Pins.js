615fde651a6b147935802e7c2c346c1fb9ac9e54
Yura_viper <yuramcpecs@gmail.com>
2022-11-24 09:33:53 +0100

    Added 'empty' class to Pins and SavedPins, added styling based on this class

--- src/components/Pins/Pins.js
+++ src/components/Pins/Pins.js
@@ -2,8 +2,9 @@ import PinOpener from "./Pin/Pin_opener/Pin_opener";
 
 function Pins({ pins, handlePinOpenerClick, handlePinSave, handlePinRemoveFromSaved }){
     return(
-        <div className="pins">
+        <div className={`pins ${pins.length === 0 ? "empty" : ""}`}>
             {
+                pins.length > 0 ? 
                 pins.map((pin) => <PinOpener 
                     pin={pin} 
                     key={`pin_${pin.id}`} 
@@ -11,7 +12,9 @@ function Pins({ pins, handlePinOpenerClick, handlePinSave, handlePinRemoveFromSa
                     handlePinSave={handlePinSave} 
                     handlePinRemoveFromSaved={handlePinRemoveFromSaved}
                     /> 
-                ) 
+                )
+                :
+                <b>No pins</b> 
             }
         </div>
     );
--- src/components/Pins/Saved_pins/Saved_pins..js
+++ src/components/Pins/Saved_pins/Saved_pins..js
@@ -5,9 +5,12 @@ function SavedPins({ savedPins, handlePinOpenerClick, handlePinSave, handlePinRe
     const userId = useSelector((state) => state.user.id);
 
     return(
-        <div className="savedPins">
+        <div className={`savedPins ${savedPins.length === 0 ? "empty" : ""}`}>
             {
+                savedPins.length > 0 ?
                 savedPins.map((savedPin) => <PinOpener userId={userId} pin={savedPin} handlePinOpenerClick={handlePinOpenerClick} handlePinSave={handlePinSave} handlePinRemoveFromSaved={handlePinRemoveFromSaved} key={`saved_pin_${savedPin.id}`} />)
+                :
+                <b>No saved tags</b>
             }
         </div>
     );
